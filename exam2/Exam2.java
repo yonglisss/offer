7edcf6505be83c6f59c44a3b0e9761b5695b792f
yonglisss <1144989424@qq.com>
2015-08-02 14:05:32 +0800

    Update Exam2.java

--- exam2/Exam2.java
+++ exam2/Exam2.java
@@ -1,1 +1,38 @@
+/**
+ * 面试题2：查找数组中的元素
+ * 问题描述：在一个按行递增，按列递增的二维数组中，查找指定元素
+ * 
+ * */
 
+public class Exam2 {
+	private static int [][] arr = {{1,2,8,9},{2,4,9,12},{4,7,10,13},{6,8,11,15}};
+	
+	public static boolean search(int a[][],int width,int high,int num){
+		boolean found = false;
+		
+		if(a != null && width > 0 && high > 0){
+			int width2 = width - 1;
+			int high2 = 0;
+			
+			while(width2 >= 0 && high2 < high){
+				if(num == a[high2][width2]){
+					found = true;
+					break;
+				}else if(num < a[high2][width2]){
+					width2--;
+				}else{
+					high2++;
+				}
+			}
+		}
+		
+		return found;
+	}
+	
+	
+	public static void main(String [] args){
+		boolean b;
+		b = search(arr,4,4,3);
+		System.out.println(b);
+	}
+}
